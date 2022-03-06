7a8ab31e808ac0bbb7d9847e55e19d9fee8f54b4
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2022-03-06 18:29:52 +0530

    StateFull  Widget Demo

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
-      home: Scaffold(
-        appBar: AppBar(
-          title:Center(child: Text("Home Page")),
-        ),
-        body: Center(
-          child: Text("Hello!"),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String myText = "Hello World!";
+
+  void _changeText() {
+    setState(() {
+      if (myText.startsWith("H")) {
+        myText = "Welcome to my App";
+      } else {
+        myText = "Hello World!";
+      }
+    });
+  }
+
+  Widget _bodyWidget() {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          children: [
+            Text(myText),
+            ElevatedButton(
+              onPressed: () {
+                _changeText();
+              },
+              child: Text("Clicked"),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: _bodyWidget(),
     );
   }
 }
